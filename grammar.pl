5ece4e9072f9aea5061f2afa3dcfe8722d7b5852
Bohdan Forostianyi <qwebeck5@gmail.com>
2020-01-11 14:14:18 +0100

    Add appropriate prepositions to verbs, add generate random clause statement

--- grammar.pl
+++ grammar.pl
@@ -1,43 +1,101 @@
 :- consult('lexic').
+:- use_module(library(aggregate)).
 
-story --> s(introduction,Time),['.'],s(hero_acts,Time).
+story --> {random_grammar_clause(2,introduction,Introduction)}, 
+	   Introduction.
+%story --> s(introduction,Time),['.'],
+%	  s(hero_acts,Time),['.'],
+%	  s(hero_acts,Time).
 
-s(introduction,Time) --> {rand_lexem([times],0,Time)}, place_p(Time).
-s(hero_acts,Time) --> hero_p(extended),action(Time). 
+%% It was a .... in ....
+%% It is a ... in ....
+%% Hero ... made ..... -> caused.
+
+introduction(place_desription,Time) --> {rand_word([times],0,Time)},
+			       place_p(Time).
+
+introduction(action,Time) --> {rand_word([times],0,Time)},
+			      s(hero_acts, Time).
+
+
+
+s(hero_acts,Time) --> {random_grammar_clause(1,hero_p,HeroPhrase)},
+		      HeroPhrase,
+		      action(Time). 
 
 %%'It is a sunny day in the Galactic Hall. Tired, Anakin learning to play the guitar'.
 %% 'Exited with new toy, Anakin learing to play the guitar'.
 % Time is a time form of action: continous, past perfect, present perfect.
-action(Time) --> vp(Time), np.
-vp(present) --> v(infinitive).
-vp(continous) --> v(continous), prep, v(infinitive).
-vp(past) --> np.
+action(Time) --> vp(Time),
+		 np.
+
+%vp(present) --> v(infinitive).
+
+%vp(continous) --> v(continous), 
+%		  prep, 
+%		  n.
+
+vp(past) --> sc(past),
+	     v(continous),
+	     prep,
+	     n.
+
 hero_p(simple) --> hero.
-hero_p(extended) --> participle_p, hero.
-place_p(Time) --> ['It'], sc(Time), article, adj(wapp), period, place_ph.
-place_ph --> prep(papp), adj(papp), place.
-place_ph --> prep(papp), place.
-participle_p --> participle ,prep, np, [','].
-np --> article, n.
+
+hero_p(extended) --> participle_p,
+		     hero.
+
+place_p(Time) --> ['It'], 
+		  sc(Time),
+		  article, 
+		  adj(wapp),
+		  period,
+		  {random_grammar_clause(1,place_ph,PlacePhrase)},
+		  PlacePhrase.
+
+place_ph(extended) --> prep(prp),
+	               adj(papp),
+             	       place.
+
+place_ph(simple) --> prep(prp),
+	     	     place.
+
+participle_p --> participle,
+		 prep,
+		 np,
+		 [','].
+
+np --> article,
+       n.
+
+
+prep --> [Word],{rand_word([prep,_],0,Word)}.
+
+prep(Target) --> [Word],{rand_word([prep,Target],0,Word)}.
+
+n --> [Word],{rand_word([n, _],0,Word)}.
+
+article --> [Word],{rand_word([article],0,Word)}.
+
+v(GrammarForm) --> {rand_lexem([v,GrammarForm,_], 0, lex(Word,_,_,PrepositionList)),
+		    connect_verb_prep(Word,PrepositionList,Result)},
+		    Result.
+
+hero --> [Word],{rand_word([hero],0,Word)}.
+
+participle --> [Word],{rand_word([participle,_],0,Word)}.
+
+adj(Target) --> [Word],{rand_word([adj,Target],0,Word)}.
+
+sc(Time) --> [Word],{rand_word([sc,Time],0,Word)}.
+
+period --> [Word],{rand_word([period],0,Word)}.
+
+place --> [Word],{rand_word([place],0,Word)}.
 
 
 
-
-prep --> [Word],{rand_lexem([prep],0,Word)}.
-prep(Target) --> [Word],{rand_lexem([prep,Target],0,Word)}.
-n --> [Word],{rand_lexem([n, _],0,Word)}.
-article --> [Word],{rand_lexem([article],0,Word)}.
-v(GrammarForm) --> [Word],{rand_lexem([v,GrammarForm], 0, Word)}.
-hero --> [Word],{rand_lexem([hero],0,Word)}.
-participle --> [Word],{rand_lexem([participle,_],0,Word)}.
-adj(Target) --> [Word],{rand_lexem([adj,Target],0,Word)}.
-sc(Time) --> [Word],{rand_lexem([sc,Time],0,Word)}.
-period --> [Word],{rand_lexem([period],0,Word)}.
-place --> [Word],{rand_lexem([place],0,Word)}.
-
-
-
-insert(Element, 0, Lista, [Element|Lista]).
+insert(Element, 0, List, [Element|List]).
 insert(Element, I, [H|T], [H|NT]) :-
     I > 0,
     NI is I - 1,
@@ -46,7 +104,7 @@ insert(Element, I, [H|T], [H|NT]) :-
 
 
 %% Wrapper around get_rand_lexem, that takes a list of ignore parametrs, as argument.
-rand_lexem(IgnoreParamsList, GoalPos, Result):-   	
+rand_word(IgnoreParamsList, GoalPos, Result):-   	
 	insert(_,GoalPos,IgnoreParamsList,NewPred),
     	Template =.. [lex|NewPred],
 	bagof(Template,IgnoreParamsList^Template, PossibleLexems),
@@ -55,7 +113,38 @@ rand_lexem(IgnoreParamsList, GoalPos, Result):-
 	nth0(I, PossibleLexems, Lexem),
 	Lexem =.. [lex|Params],
 	nth0(GoalPos,Params,Result).
-	
+
+rand_lexem(IgnoreParamsList, GoalPos, Result):-   	
+	insert(_,GoalPos,IgnoreParamsList,NewPred),
+    	Template =.. [lex|NewPred],
+	bagof(Template,IgnoreParamsList^Template, PossibleLexems),
+	length(PossibleLexems, L),
+	random(0, L, I),
+	nth0(I, PossibleLexems, Result).
 
 
+connect_verb_prep(Verb,PrepList,[Verb,Prep]):-
+	length(PrepList,L),
+	random(0,L,I),
+	nth0(I,PrepList,Prep).
+
+
+%% Returns random grammar clause
+%% ArgCount - number of arguments, grammmar clause accepts (without undeneath differential list)
+%% Clause - name of grammar clause 
+random_grammar_clause(ArgCount, Predicate, Result):-
+	C is ArgCount + 2,
+	length(Args, C),
+	GrammarC =.. [Predicate|Args],
+	aggregate(count,Args^GrammarC,NumberOfPossibilities),
+	random_between(1,NumberOfPossibilities, I),	
+	nth_clause(GrammarC, I, R), clause(ClauseWithDiffList, _, R),
+	remove_diff_list_from_clause(ClauseWithDiffList,Result).
+
+
+remove_diff_list_from_clause(ClauseWithDiff, Result):-
+	ClauseWithDiff =.. [Predicate|Params],
+	length(DiffList, 2),
+	append(ParamsWithoutDiffList, DiffList, Params),
+	Result =.. [Predicate|ParamsWithoutDiffList].
 
--- lexic.pl
+++ lexic.pl
@@ -8,35 +8,71 @@ lex(past, times).
 lex(day, period).
 lex(night, period).
 lex(morning, period).
+lex(midnight, period).
+lex(afternoon, period).
+lex(teatime, period).
+lex(siesta, period).
+lex(halloween, period).
 
 % Heros
 lex('John Snow',hero).
 lex('Anakin',hero).
 lex('Obi-Wan',hero).
 lex('Unicorn',hero).
+lex('Ragnar',hero).
+lex('Snowman',hero).
 
+
+% wapp - weather adjective could be applied
+% papp - place adjectives could be applied 
 %Places
 lex('hall', place).
-lex('forest',place).
 lex('ship',place).
 lex('castle',place).
+lex('forest',place).
+lex('mountains',place).
+lex('Britan', place).
+lex('Island', place).
 
 % Articles
 lex('the',article).
 lex('a',article).
 
-% papp - place appliable
+
+% prp - preposition of place
+% prt - preposition of time
+% prm - preposition of movement
 % Prepositions.
-lex('to',prep).
-lex('on',prep).
-lex('about',prep).
-lex('with',prep).
-lex('in',prep,papp).
-lex('under',prep,papp).
-lex('on',prep,papp).
+%prt
+lex('on',prep,prt).
+lex('in',prep,prt).
+lex('at',prep,prt).
+lex('before',prep,prt).
+lex('after',prep,prt).
+%prp
+lex('at',prep,prp).
+lex('on',prep,prp).
+lex('in',prep,prp).
+lex('inside',prep,prp).
+lex('under',prep,prp).
+%prm
+lex('to',prep,prm).
+lex('across',prep,prm).
+lex('into',prep,prm).
+lex('through',prep,prm).
+ 
+%lex('to',prep).
+%lex('on',prep).
+%lex('about',prep).
+%lex('with',prep).
+%lex('in',prep,papp).
+%lex('under',prep,papp).
+%lex('on',prep,papp).
+
 
 % Sentence connectors
 lex('was',sc,past).
+lex('has been', sc, past).
 lex('is',sc, present).
 lex('is',sc, continous).
 
@@ -44,12 +80,19 @@ lex('is',sc, continous).
 lex('guitar',n,s).
 lex('football',n,s).
 lex('magic',n,s).
+lex('sword',n,s).
+lex('north',n,s).
+lex('book',n,s).
+lex('shield',n,s).
+lex('hat',n,s).
+lex('backpack',n,s).
+lex('goblet',n,s).
 
+% Adjectives 
 % _ - universal adjectives 
 % human appliable - happ.
 % weather appliable - wapp.
 % place appliable - papp
-% Adjectives 
 lex('giggling',adj,happ).
 lex('cloudy',adj,wapp).
 lex('boring',adj,_).
@@ -60,15 +103,6 @@ lex('space',adj,papp).
 lex('medival',adj,papp).
 lex('lonely',adj,_).
 
-
-
-% Verbs
-lex('play',v,infinitive).
-lex('learn',v,infinitive).
-lex('puffing',v,continous).
-lex('learning',v,continous).
-lex('playing',v,continous).
-
 % human_appliable - happ
 % Participles
 lex('risen',participle,nhapp).
@@ -77,7 +111,16 @@ lex('exited',participle,happ).
 lex('interested',participle,happ).
 
 % Participle
-lex('giggling',participle,happ,continous).
 
 
+% Format
+% lex(word, v, time, list_of_related_prepositions)
+% list_of_related_prepositions = [with,at, ...]
+% Verbs
+lex('play',v,infinitive,[with, at]).
+lex('learn',v,infinitive,[with, at]).
+lex('puffing',v,continous,[at, on, out]).
+lex('learning',v,continous, [about, to]).
+lex('playing',v,continous, [with, at]).
+lex('giggling',v,continous,[at, about]).
 
